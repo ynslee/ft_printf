67469df086292f2e04aeb2578fdf2f90163d9f61
chuchilini <vanesalees@gmail.com>
2022-12-05 18:43:58 +0200

    .

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: yoonslee <yoonslee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 12:07:28 by yoonslee          #+#    #+#             */
-/*   Updated: 2022/12/05 18:37:58 by yoonslee         ###   ########.fr       */
+/*   Updated: 2022/12/05 18:43:51 by yoonslee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,29 +41,6 @@ int	ft_check(char c, va_list ap)
 		length += ft_printchr('%');
 	return (length);
 }
-
-// int	ft_formats(va_list args, const char format)
-// {
-// 	int	print_length;
-
-// 	print_length = 0;
-// 	if (format == 'c')
-// 		print_length += ft_printchar(va_arg(args, int));
-// 	else if (format == 's')
-// 		print_length += ft_printstr(va_arg(args, char *));
-// 	else if (format == 'p')
-// 		print_length += ft_print_ptr(va_arg(args, unsigned long long));
-// 	else if (format == 'd' || format == 'i')
-// 		print_length += ft_printnbr(va_arg(args, int));
-// 	else if (format == 'u')
-// 		print_length += ft_print_unsigned(va_arg(args, unsigned int));
-// 	else if (format == 'x' || format == 'X')
-// 		print_length += ft_print_hex(va_arg(args, unsigned int), format);
-// 	else if (format == '%')
-// 		print_length += ft_printpercent();
-// 	return (print_length);
-// }
-
 
 int	ft_printf(const char *str, ...)
 {
